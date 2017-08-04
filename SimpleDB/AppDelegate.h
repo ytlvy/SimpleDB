ba22d8f7325d78445af8ae92ce766a9b17436251
gyj <gyj@gyjdeMacBook-Pro.local>
2017-08-04 14:46:31 +0800

    init

---
+++ SimpleDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleDB
+//
+//  Created by gyj on 2017/8/4.
+//  Copyright © 2017年 com.ytlvy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleDB
+//
+//  Created by gyj on 2017/8/4.
+//  Copyright © 2017年 com.ytlvy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
